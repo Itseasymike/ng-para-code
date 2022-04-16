37a87e7e724eb439409b985ff8e9ca797fcec9c1
Michael Rivera <mike@Michaels-MBP.attlocal.net>
2022-04-16 11:11:37 -0500

    user mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MockDataService} from './services/mock-data.service';
 import {HttpErrorResponse} from '@angular/common/http';
 import {User} from './models/user-data.interface';
@@ -8,12 +8,40 @@ import {User} from './models/user-data.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   data: User[] | undefined;
+  displayedColumns: string[] = ['name', 'email', 'address', 'phone'];
+  dataSource: any;
 
   constructor(private dataService: MockDataService) {
 
+  }
+
+  ngOnInit() {
+    this.dataService.api.pageData
+    .users()
+    .subscribe({
+      next: (userData: User[]) => {
+        this.data = userData;
+        // structure needed for mat table
+        this.dataSource = userData.map((user) => (
+          { id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            address: user.address,
+            phone: user.phone,
+            website: user.website,
+            company: user.company,
+
+          }
+        ));
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log('Error =>', error);
+      }
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {MockDataService} from './services/mock-data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatTableModule,
     BrowserAnimationsModule,
   ],
   providers: [
